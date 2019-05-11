3362b11a35c21842e52889ead444e0b3cadc7838
Hosmos <375938415@qq.com>
2019-05-11 10:52:55 +0800

    no message

--- java-learning-management/src/main/resources/sql/t_mail.sql
+++ java-learning-management/src/main/resources/sql/t_mail.sql
@@ -7,8 +7,8 @@ DROP TABLE IF EXISTS `t_mail`;
 CREATE TABLE `t_mail` (
   `id` int(11) NOT NULL AUTO_INCREMENT COMMENT '主键',
   `userId` int(11) NOT NULL COMMENT '发送人',
-  `subject` varchar(255) CHARACTER SET utf8mb4mb4 COLLATE utf8mb4mb4_general_ci NOT NULL COMMENT '标题',
-  `content` longtext CHARACTER SET utf8mb4mb4 COLLATE utf8mb4mb4_general_ci NOT NULL COMMENT '正文',
+  `subject` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '标题',
+  `content` longtext CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL COMMENT '正文',
   `createTime` datetime NOT NULL COMMENT '创建时间',
   `gmtTime` datetime NOT NULL COMMENT '修改时间',
   PRIMARY KEY (`id`),
--- java-learning-management/src/main/resources/sql/t_mail_to.sql
+++ java-learning-management/src/main/resources/sql/t_mail_to.sql
@@ -9,7 +9,7 @@ CREATE TABLE `t_mail_to` (
   `mailId` int(11) NOT NULL COMMENT '邮件编码',
   `toUser` varchar(128) NOT NULL COMMENT '接收人',
   `status` tinyint(1) NOT NULL DEFAULT '1' COMMENT '1成功，0失败',
-  `createTime` datetime NOT NULL COMMENT '创建时间'
+  `createTime` datetime NOT NULL COMMENT '创建时间',
   PRIMARY KEY (`id`),
   KEY `index_mailId` (`mailId`),
   KEY `index_toUser` (`toUser`)
